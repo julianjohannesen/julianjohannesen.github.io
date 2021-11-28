6869859b66aaa15ff033b83de177d319ea6e4e17
julianjohannesen <julian.johannesen@gmail.com>
2021-11-27 21:46:20 -0500

    messing around with bulma

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {Fragment} from 'react';
 import Home from './pages/Home';
+import BulmaCheatsheet from '../ideas/BulmaCheatsheet';
 
 export default function App() {
     return (
         <Fragment>
-            <Home />
+            <BulmaCheatsheet />
         </Fragment>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// import '../public/debug.css';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'; 
 import '../public/vendor/bulma.min.css';
+import '../public/custom.css';
 
 ReactDOM.render( 
     <React.StrictMode >
-        <App / >
+        <App />
     </React.StrictMode>,
     document.getElementById('root')
 );
