fe88f0e92d51a35db667bfba06e40cd89d9138ae
julianjohannesen <julian.johannesen@gmail.com>
2021-12-01 21:27:37 -0500

    added more Bulma components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, {Fragment} from 'react';
 import Home from './pages/Home';
 import BulmaCheatsheet from '../ideas/BulmaCheatsheet';
 import Columns from '../ideas/ColumnTest';
+import BulmaBlog from '../ideas/BulmaBlog';
 
 export default function App() {
     return (
         <Fragment>
-            <Columns/>
+            <BulmaBlog/>
         </Fragment>
     )
 }
